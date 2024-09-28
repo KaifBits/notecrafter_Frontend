4a158d536e420d4dd9450e02259b5a8277b2786a
Sk Kaif Rahaman <skkaifrahaman112@gmail.com>
2024-09-28 15:32:31 +0530

    Initial commit

--- src/Signup.js
+++ src/Signup.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import {Link } from 'react-router-dom';
 import axios  from 'axios';
+import img from "./components/utils/logo.png";
 
 const Singup = () => {
    const[data,setData]=useState({
@@ -71,6 +72,7 @@ const Singup = () => {
             
             NoteCrafter    
         </Link>
+        <img src={img} alt="Logo" className='size-16 left-40' />
         <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
                 <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
